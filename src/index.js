bdbedd9be3ffd62a56e26709b9eadf1c79391ab4
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2018-01-27 14:27:29 -0200

    naive solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-exports.calculateMaxTotal = function () {
-  return 26;
+exports.calculateMaxTotal = function (triangle = []) {
+  let maxGlobal = 0;
+  let upperIndex = -1;
+
+  triangle.forEach((row) => {
+    if (row.length === 1) {
+      [maxGlobal] = row;
+      upperIndex = 0;
+    } else if (row[upperIndex + 1] > row[upperIndex]) {
+        maxGlobal += row[upperIndex + 1];
+        upperIndex += 1;
+      } else {
+        maxGlobal += row[upperIndex]
+      }
+  });
+
+  return maxGlobal;
 }
