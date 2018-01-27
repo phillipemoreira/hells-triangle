584d034a85d45ce5e33ec1fc3bd161be50186cf2
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2018-01-27 13:22:00 -0200

    linting

--- index.test.js
+++
@@ -1,9 +0,0 @@
-const calculateMaxTotal = require('./index').calculateMaxTotal;
-
-console.log(calculateMaxTotal());
-
-describe('CalculateMaxTotal', () => {
-  it('should match initial test case', () => {
-    expect(calculateMaxTotal()).toEqual(26);
-  });
-});
