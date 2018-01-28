4e725bb5b7771f1759bb630cbf279fe7d1d847e3
Phillipe Moreira <phillipealexmoreira@gmail.com>
2018-01-28 20:08:21 -0200

    Solution (#2)
    * coverage report
    
    * naive solution
    
    * Dijkstra reverso
    
    * updating tests
    
    * splitting utils file
    
    * Updating readme
    
    * lint rules

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,41 @@
 const {calculateMaxTotal} = require("./index");
 
 describe("CalculateMaxTotal", () => {
-  it("should match initial test case", () => {
-    expect(calculateMaxTotal()).toEqual(26);
+  it("empty triangle", () => {
+    expect(calculateMaxTotal()).toEqual(0);
+  });
+
+
+  it("initial test case", () => {
+    const triangle = [
+      [6],
+      [3, 5],
+      [9, 7, 1],
+      [4, 6, 8, 4]
+    ];
+
+    expect(calculateMaxTotal(triangle)).toEqual(26);
+  });
+
+
+  it("when following the local max leads to the global max", () => {
+    const triangle = [
+      [3],
+      [7, 2],
+      [9, 8, 4]
+    ];
+
+    expect(calculateMaxTotal(triangle)).toEqual(19);
+  });
+
+
+  it("when following the local max does not lead to the global max", () => {
+    const triangle = [
+      [2],
+      [1, 3],
+      [10, 2, 4]
+    ];
+
+    expect(calculateMaxTotal(triangle)).toEqual(13);
   });
 });
